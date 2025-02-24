fae92afc66c29bf5e3935031d860e1a5e936f106
egosection <egosection@gmail.com>
2025-02-24 13:45:00 +0200

    header v3 backup

--- src/js/main.js
+++ src/js/main.js
@@ -6,30 +6,36 @@ const hamburgerCloseSvg = document.querySelector('#hamburger-close svg');
 const navMenu = document.querySelector('#menu-holder');
 const navLinks = document.querySelectorAll('#menu-holder ul li a');
 const overlay = document.querySelector('#overlay');
+const searchBtn = document.querySelector('.search-btn');
+const searchWrap = document.querySelector('.search-wrap');
 
-const openMobileMenu = () => {
+function openMobileMenu() {
     navMenu.classList.add('right-[0]');
     hamburgerBtn.classList.add('hidden');
     hamburgerCloseBtn.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-    toggleOverlayAria();
+    openOverlay();
 };
 
-const closeMobileMenu = () => {
+function closeMobileMenu() {
     navMenu.classList.remove('right-[0]');
     hamburgerBtn.classList.remove('hidden');
     hamburgerCloseBtn.classList.add('hidden');
-    overlay.classList.add('hidden');
-    toggleOverlayAria();
+    closeOverlay();
+    hamburgerSvg.classList.add('scale');
 };
 
-const toggleOverlayAria = () => {
-    let overlayAriaHidden = overlay.getAttribute("aria-hidden");
-    overlayAriaHidden === 'true' ? overlay.setAttribute('aria-hidden', 'false') : overlay.setAttribute('aria-hidden', 'true');
-};
+function openOverlay() {
+    overlay.setAttribute('aria-hidden', 'false');
+    overlay.classList.remove('hidden');
+}
+
+function closeOverlay() {
+    overlay.setAttribute('aria-hidden', 'true');
+    overlay.classList.add('hidden');
+}
 
 //Add disabled attribute to html to prevend bug when we click too fast during transition
-const disableButtons = () => {
+function disableButtons() {
     hamburgerBtn.disabled = true;
     hamburgerCloseBtn.disabled = true;
     setTimeout(() => {
@@ -38,22 +44,44 @@ const disableButtons = () => {
     }, 100);
 };
 
+function disableSearchButtons() {
+    searchBtn.classList.add('cursor-none');
+    setTimeout(() => {
+        searchBtn.classList.add('cursor-pointer');
+    }, 200);
+};
+
+function openSearch() {
+    searchWrap.classList.remove('top-[-100%]');
+    searchWrap.classList.add('top-[20%]');
+}
+
+function closeSearch() {
+    searchWrap.classList.remove('top-[20%]');
+    searchWrap.classList.add('top-[-100%]');
+}
+
+function toggleSearch() {
+    closeMobileMenu();
+    searchWrap.classList.toggle('top-[20%]'); 
+}
+
+
 hamburgerBtn.addEventListener('click', () => {
+    closeSearch();
     hamburgerCloseSvg.classList.add('rotate180');
     openMobileMenu();
     disableButtons();
 });
 
 hamburgerCloseBtn.addEventListener('click', () => {
-    hamburgerSvg.classList.add('scale');
     closeMobileMenu();
     disableButtons();
 });
 
 overlay.addEventListener('click', () => {
-    hamburgerSvg.classList.add('scale');
     closeMobileMenu();
-    disableButtons();
+    closeOverlay();
 });
 
 navLinks.forEach((link) => {
@@ -64,4 +92,15 @@ navLinks.forEach((link) => {
         hamburgerCloseBtn.classList.add('hidden');
         overlay.classList.add('hidden');
     })
-})
+});
+
+searchBtn.addEventListener('click', () => {
+    if(searchWrap.classList.contains('top-[-100%]')) {
+        searchWrap.classList.remove('top-[-100%]');
+        searchWrap.classList.add('top-[20%]');
+        closeMobileMenu(); 
+    } else {
+        searchWrap.classList.remove('top-[20%]');
+        searchWrap.classList.add('top-[-100%]');
+    } 
+});
